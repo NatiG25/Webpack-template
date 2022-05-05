c8c7b9161266a14730c62ba661e093d2e8597e7e
NatiG <nati.g5684@gmail.com>
2022-05-05 19:44:01 +0300

    Add Webpack  & proper README file

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const component = () => {
+    const element = document.querySelector('.test');
+    element.innerHTML = 'Welcome';
+  };
+
+window.onload = component();
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const component = () => {
+    const element = document.querySelector('.test');
+    element.innerHTML = 'Welcome';
+  };
+
+window.onload = component();
